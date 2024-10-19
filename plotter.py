7291e436d49f0555fec7a91569776e79e0e82eb8
carlesmatoses <carlesmatosesg@gmail.com>
2024-10-19 16:41:52 +0200

    graphs update

--- plotter.py
+++ plotter.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+plt.style.use('bmh')
 
 pixels = [256, 512, 1024, 2048]
 methods = ["openmp", "mpi", "cuda"]
@@ -27,11 +28,49 @@ tests["openmp"]["jacobi"][16]["y"] = [0.450, 1.831, 9.097, 110.765]
 # tests["mpi"]["gauss_seidel"][4]["y"] = [15, 25, 35, 45]
 
 # Plotting
+line_256 = [[2.392, 1.248,  0.714, 0.488, 0.450],
+            [1,2,4,8,16],"256"]
+line_512 = [[20.846, 10.939, 5.001, 2.804, 1.831],
+            [1,2,4,8,16],"512"]
+line_1024 = [[140.121, 76.065, 34.738, 19.375, 9.097],
+             [1,2,4,8,16],"1024"]
+line_2048 = [[271.630, 163.013, 110.765],
+             [4,8,16],"2048"]
+
+# execution time
 plt.figure()
-for thread in threads:
-    plt.plot(tests["openmp"]["jacobi"][thread]["x"], tests["openmp"]["jacobi"][thread]["y"], label=f'Threads {thread}')
+for line in [line_256, line_512, line_1024, line_2048]:
+    x = line[1]
+    y = line[0]
+    plt.plot(x, y, label=f'Resolution {line[2]}', marker='o')
 plt.title('OpenMP Jacobi Solver')
-plt.xlabel('Pixels^2')
-plt.ylabel('Time')
+plt.xlabel('Number of Threads')
+plt.ylabel('Execution Time')
+plt.legend()
+plt.show()
+
+# speedup
+line_perfect = [[1, 2, 4, 8, 16],[1,2,4,8,16],"Perfect"]
+plt.figure()
+for line in [line_256, line_512, line_1024]:
+    x = line[1]
+    y = [line[0][0]/val for val in line[0]]
+    plt.plot(x, y, label=f'Resolution {line[2]}', marker='o')
+plt.plot(line_perfect[0], line_perfect[0], linestyle='dotted', color='black')
+plt.title('OpenMP Jacobi Solver Speedup')
+plt.xlabel('Number of Threads')
+plt.ylabel('Speedup')
+plt.legend()
+plt.show()
+
+# efficiency
+plt.figure()
+for line in [line_256, line_512, line_1024]:
+    x = line[1]
+    y = [line[0][0]/(Tp*P) for Tp, P in zip(line[0],line[1])]
+    plt.plot(x, y, label=f'Resolution {line[2]}', marker='o')
+plt.title('OpenMP Jacobi Solver Efficiency')
+plt.xlabel('Number of Threads')
+plt.ylabel('Efficiency')
 plt.legend()
 plt.show()
